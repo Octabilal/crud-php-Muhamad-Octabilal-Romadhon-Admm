51ebcd3ec24482f54f3dc8563627820f9b32f9d4
Octabilal <muhamadoctabilalramadan@gmail.com>
2024-11-20 11:43:50 +0700

    Add files via upload

---
+++ hapus_mahasiswa.php
@@ -0,0 +1,17 @@
+<?php
+include 'koneksi.php';
+$id = $_GET['id'];
+
+$id = intval($id);
+
+$sql = "DELETE FROM mahasiswa WHERE id=$id";
+
+if ($conn->query($sql) === TRUE) {
+    header("Location: index.php");
+    exit;
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "kampus";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+?>
